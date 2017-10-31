224593bb1750960a2d05ac6c4589ebedc034dd28
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-10-31 14:52:29 -0700

    init commit

---
+++ frontend/js/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+
+
+
